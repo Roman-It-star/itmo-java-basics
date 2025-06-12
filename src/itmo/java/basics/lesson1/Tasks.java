09bf3ec2b1e7652d809291a981a67f12989b0488
Roman-It-star <romanop2014@gmail.com>
2025-06-12 12:11:53 +0300

    lab 1

---
+++ src/itmo/java/basics/lesson1/Tasks.java
@@ -0,0 +1,12 @@
+package itmo.java.basics.lesson1;
+
+public class Tasks {
+    public static void main(String[] args){
+        //
+    }
+
+    public void taskMethod(){
+        //logic
+    }
+}
+
